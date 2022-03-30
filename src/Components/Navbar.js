b59f4cfeaf847ba2c96b73553327d6dbe5baad12
unknown <montherismail90@gmail.com>
2022-03-30 10:58:48 +0300

    Create Navbar Component And Style It Relates #2

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <nav>
+            <h2>Manege Your Tasks</h2>
+            <div className="dark-add">
+                <img src="#" alt="Dark-mode Icon"/>
+                <button>Add Task</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
